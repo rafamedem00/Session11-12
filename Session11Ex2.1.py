59de82ae5296890a92c58cc6af12b3ecdc801c20
rafa <rafamedem@hotmail.com>
2019-10-08 11:17:24 +0200

    input 4 numbers and determine the average

---
+++ Session11Ex2.1.py
@@ -0,0 +1,11 @@
+#input 4 numbers and determine the average
+i = 0
+b = []
+while(i<4):
+    try:
+        n = input('give me a number')
+        n = int(n)
+        b.append(n)
+        i = i+1
+    except ValueError:
+        print('please enter a number')
